35a3f9f3bae3df507c7fd87f9168ad61e98dd823
bgavilanezb <bigavilanezb00@gmail.com>
2025-06-05 20:21:43 +0200

    Ejercicios teminados

--- javascript/prueba.js
+++ javascript/prueba.js
@@ -201,6 +201,26 @@ for (let i = 0; i <= 100; i++) {
 }
 
 for (let i = 0; i <= 20; i++) {
-  if(i>5 && i<7)
-    if(i==5 || i== 3)
-} console.log(i)
+  if ( i % 3 === 0 && i % 5 === 0) {
+    console.log('FizzBuzz')
+  } else if (i % 3 === 0) {
+    console.log('Fizz')
+  } else if (i % 5 === 0) {
+    console.log('Buzz')
+  } else {
+    console.log(i)
+  }
+}
+
+let suma = 0;
+for (let i = 5; i >= 0; i-- ){
+  suma += i;
+}
+
+console.log(suma)
+
+const number1 = 7;
+const number2 = 0;
+for (let i = 1; i <= 10; i++) {
+  console.log('7 * ' + i + ' = ' + number1 * i)
+}
